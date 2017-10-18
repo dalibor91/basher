9c4f79eface3ef5c8a44712b96277efb4eae6b88
dalibor91 <dalibor_menkovic@hotmail.com>
2017-10-18 11:29:55 +0200

    Update git_pull_all_branches.sh

--- scripts/git_pull_all_branches.sh
+++ scripts/git_pull_all_branches.sh
@@ -1,2 +1,4 @@
+#!/bin/bash
+
 for i in $(git branch -r | grep -v 'origin/HEAD'); do branch=$(echo $i | awk '{ a=$1; gsub("origin/", "", a); print a }'); git checkout $branch; git pull; done
 git checkout master

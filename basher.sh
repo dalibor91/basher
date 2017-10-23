53eac475dfc87de87aed020afe6d1aa021107bc4
dalibor91 <dalibor_menkovic@hotmail.com>
2017-10-23 12:32:50 +0200

    major changes

--- basher.sh
+++ basher.sh
@@ -1,223 +1,298 @@
 #!/bin/bash
 
-BASER_DIR=$(echo ~)
-BASER_DIR="${BASER_DIR}/.basher"
 
-function dieOnFail() {
-	if [ ! $1 -eq 0 ];
+options_add=""
+options_delete=""
+options_remote=""
+options_run=""
+options_explain=""
+options_list=0
+options_help=0
+options_update=0
+options_home_dir="${HOME}/.bshr"
+options_scripts_dir="${options_home_dir}/scripts"
+options_aliases_dir="${options_home_dir}/aliases"
+options_describ_dir="${options_home_dir}/describe"
+options_global_log="${options_home_dir}/log"
+options_args=""
+
+function _log() {
+    local msg=$(date +"[ %Y-%m-%d %H:%M:%S ] ")
+    local msg="${msg}${1}"
+
+    echo "${msg}" >> "${options_global_log}"
+}
+
+function _dieOnFail() {
+    _log "_dieOnFail [ $1 ] [ $2 ]"
+    if [ ! $1 -eq 0 ];
 	then
-		if [ "$2" == "" ];
-		then
-			echo "$2"
-		fi
-		exit 1
+		if [ ! "$2" = "" ]; then echo "$2"; fi
+		exit 1;
 	fi
 }
 
-function check() {
-	if [ ! -d "${1}/.basher" ];
-	then
-		mkdir "${1}/.basher"
-		dieOnFail $? "Unable to create local dir"
-	fi
-
-        if [ ! -d "${1}/.basher/scripts" ];
+function _parseParams() {
+    while [[ $# -gt 0 ]];
+    do
+        local key=$1
+        _log "key passed -> ${key}"
+        if [ "${key}" = "--args" ];
         then
-                mkdir "${1}/.basher/scripts"
-		dieOnFail $? "Unable to create scripts directory"
-        fi
-
-        if [ ! -d "${1}/.basher/aliases" ];
-        then
-                mkdir "${1}/.basher/aliases"
-		dieOnFail $? "Unable to create aliases directory"
-        fi
-}
-
-
-function fetch() {
-	if [ ! "$1" == "" ];
-	then
-		local date=$(date +"%Y_%m_%d_%H_%M_%S")
-		local log_path="${BASER_DIR}/scripts/${date}.log"
-		local scr_path="${BASER_DIR}/scripts/${date}.sh"
-
-		echo "Downlaoded from" >> "$log_path"
-		echo $1 >> "$log_path"
-		echo "--------------------------" >> "$log_path"
-		echo "Description: "
-		read -r entered_desc
-		echo $entered_desc >> "${log_path}"
-
-		wget -O "${scr_path}" $1
-		dieOnFail $? "Unable to download script"
-
-		file_alias="-"
-		until [[ ! "$file_alias" =~ [^a-zA-Z0-9_] ]];
-		do
-			echo "Enter command name to run like:"
-			read -r file_alias
-			if [ -f "${BASER_DIR}/aliases/${file_alias}" ];
-			then
-				echo "Alias already exists"
-			fi
-		done
-
-
-		ln -s "${scr_path}" "${BASER_DIR}/aliases/${file_alias}"
-		dieOnFail $? "Unable to create symbolic link"
-	fi;
-}
-
-function fetch_local() {
-        if [ -f "$1" ];
-        then
-                local date=$(date +"%Y_%m_%d_%H_%M_%S")
-                local log_path="${BASER_DIR}/scripts/${date}.log"
-                local scr_path="${BASER_DIR}/scripts/${date}.sh"
-
-                echo "Copied from" >> "$log_path"
-                echo $1 >> "$log_path"
-                echo "--------------------------" >> "$log_path"
-                echo "Description: "
-                read -r entered_desc
-                echo $entered_desc >> "${log_path}"
-
-                cp "$1" "${scr_path}"
-                dieOnFail $? "Unable to download script"
-
-                file_alias="-"
-                until [[ ! "$file_alias" =~ [^a-zA-Z0-9_] ]];
-                do
-                        echo "Enter command name to run like:"
-                        read -r file_alias
-                        if [ -f "${BASER_DIR}/aliases/${file_alias}" ];
-                        then
-                                echo "Alias already exists"
-                        fi
-                done
-
-
-                ln -s "${scr_path}" "${BASER_DIR}/aliases/${file_alias}"
-                dieOnFail $? "Unable to create symbolic link"
+            shift
+            options_args=$@
+            break
         fi;
+
+        case $key in 
+            -a|--add)
+               options_add=$2
+               shift
+               shift
+               ;;
+            -d|--delete)
+               options_delete=$2
+               shift
+               shift
+               ;;
+            -e|--explain)
+               options_explain=$2
+               shift
+               shift
+               ;; 
+            -H|--host)
+               options_remote=$2
+               shift
+               shift
+               ;;
+            -r|--run)
+               options_run=$2
+               shift
+               shift
+               ;;
+            -l|--list)
+               options_list=1
+               shift
+               ;;
+            -h|--help)
+               options_help=1
+               shift
+               ;;
+            --update)
+               options_update=1
+               shift
+               ;;
+            *)
+                #unknown 
+                #options_args="${options_args}${key} "
+                echo "Unknown -> ${key}"
+                shift
+            ;;
+        esac
+    done;
 }
 
+function _checkDirectories() {
+    if [ ! -d "$options_home_dir" ]; then mkdir "$options_home_dir"; _dieOnFail $? "Unable to create $options_home_dir"; fi;
+    if [ ! -d "$options_scripts_dir" ]; then mkdir "$options_scripts_dir"; _dieOnFail $? "Unable to create $options_scripts_dir"; fi;
+    if [ ! -d "$options_aliases_dir" ]; then mkdir "$options_aliases_dir"; _dieOnFail $? " Unable to create $options_aliases_dir"; fi;
+    if [ ! -d "$options_describ_dir" ]; then mkdir "$options_describ_dir"; _dieOnFail $? " Unable to create $options_describ_dir"; fi;
+}
 
-function get_log() {
-        if [ ! $1 == "" ];
+function _addNewScript() {
+    _log "_addNewScript()"
+
+    local file_alias="-"
+    until [[ ! "$file_alias" =~ [^a-zA-Z0-9_] ]];
+    do
+        echo "Enter alias:"
+        read -r file_alias
+        if [ -f "${options_aliases_dir}/${file_alias}" ];
         then
-                local al_loc="${BASER_DIR}/aliases/${1}"
-                if [ -f "${al_loc}" ];
-                then
-                        local abs_path=$(readlink "$al_loc")
-			local abs_path=$(basename "$abs_path")
-                        local file_name="${abs_path%.*}"
-                        if [ -f "${BASER_DIR}/scripts/${file_name}.log" ];
-			then
-				echo "${BASER_DIR}/scripts/${file_name}.log"
-			fi
-                fi
+                echo "Alias already exists"
+                local file_alias="-"
         fi
+    done
+
+    local descr="${options_describ_dir}/${file_alias}"
+
+    echo "Added from ${2}" >> "${descr}"
+
+    echo "Description: "
+    read _descr
+
+    echo "${_descr}" >> "${descr}"
+
+    ln -s "$1" "${options_aliases_dir}/${file_alias}" 
+
+    _dieOnFail $? "Unable to add alias"
+    
 }
 
-function explain() {
-
-	AL=$(get_log $1)
-
-	if [ ! "$AL" == "" ];
-	then
-		cat "$AL";
-	fi
+function _getAlias() {
+    _log "_getAlias()"
+    if [ -f "${options_aliases_dir}/$1" ]; then echo $(readlink "${options_aliases_dir}/$1"); fi;
 }
 
-function remove() {
-	local log_file=$(get_log $1)
-	if [ -f "${log_file}" ];
-	then
-		rm "${log_file}"
-		local scr_file=$(readlink "${BASER_DIR}/aliases/$1")
-		rm "${BASER_DIR}/aliases/$1"
-		rm "${scr_file}"
-		echo "Removed!"
-	else
-		echo "Not found!"
-	fi;
+function _process() {
+
+    function _action_help() { 
+        _log "Action help()"
+        echo "BSHR is small script that enables you running and managing shell scripts with one command
+    bshr
+        -a|--add     <script>     - add script to basher, local or remote
+        -r|--remove  <alias>      - run script that you saved by some alias 
+        -H|--host    <user@host>  - destination where to run script
+        -e|--explain <alias>      - print description message
+        --update                  - update basher
+        -l                        - list all scripts
+        -h                        - this message
+"; 
+    }
+
+    function _action_add() {
+        _log "Action add()"
+        if [ "$1" = "" ]; then echo "Nothing to add"; exit 2; fi;
+
+        local script_path=$1
+        local file_name=$(date +"%Y_%m_%d_%H_%M_%S.sh")
+        
+        if [[ "$script_path" =~ ^https?: ]];
+        then 
+            local _curl=$(which curl)
+            local _wget=$(which wget)
+
+            if [ "${_curl}${_wget}" = "" ]; then echo "Curl or wget are needed for download"; exit 3; fi;
+
+            if [ ! "${_curl}" = "" ];
+            then 
+                _log "Add via curl to ${file_name}" 
+                curl -s $1 > "${options_scripts_dir}/${file_name}"; _dieOnFail $? "Unable to download from $2";
+                _addNewScript "${options_scripts_dir}/${file_name}" "$1"
+            elif [ ! "$_wget" = "" ];
+            then 
+                _log "Add via wget to ${file_name}"
+                wget -O "${options_scripts_dir}/${file_name}" $1; _dieOnFail $? "Unable to download from $2"
+                _addNewScript "${options_scripts_dir}/${file_name}" "$1"
+            fi;
+        else
+            if [ ! -f "$1" ]; then "File $1 does not exissts"; exit 5; fi;
+
+            cp "$1" "${options_scripts_dir}/${file_name}"
+            _dieOnFail $? "Unable to copy $1"
+            _addNewScript "${options_scripts_dir}/${file_name}" "$1"
+        fi;
+    }
+
+    function _action_list() {
+        _log "Action list()"
+        echo "Scripts found: "
+        for item in $(ls "${options_aliases_dir}"); do echo " - ${item}"; done;
+    }
+
+    function _action_remove() {
+        _log "Action remove()"
+        local file_alias=$1
+        local script=$(_getAlias $file_alias)
+        _log "Remove ${script}"
+        if [ "$script" = "" ]; then echo "Unable to find alias"; exit 6; fi
+        _log "${options_aliases_dir}/${file_alias}"
+        rm "${options_aliases_dir}/${file_alias}"
+        _log "rm $script"
+        rm "$script"
+        if [ -f "${options_describ_dir}/${file_alias}" ]; 
+        then 
+            _log "${options_describ_dir}/${file_alias}"
+            rm "${options_describ_dir}/${file_alias}"
+        fi
+    }
+
+    function _action_explain() {
+        _log "Action explain()"
+        local file_alias=$1
+        if [ -f "${options_describ_dir}/${file_alias}" ]; 
+        then 
+            _log "${options_describ_dir}/${file_alias}"
+            cat "${options_describ_dir}/${file_alias}"
+        fi
+    }
+
+    function _action_remote() {
+        _log "Action remote(${1} ${2})"
+        local script=$(_getAlias $1)
+        if [ "${script}" = "" ]; then echo "Alias not found"; exit 7; fi;
+        if [ "$2" = "" ]; then echo "Remote server not set"; exit 9; fi;
+        if [ "`which ssh`" = "" ]; then echo "ssh command not found"; exit 8; fi;
+
+        ssh "$2" "bash -s" -- < "${script}" "${options_args}"
+    }
+
+    function _action_run() {
+        _log "Action run(${1})"
+        local script=$(_getAlias $1)
+        if [ "${script}" = "" ]; then echo "Alias not found"; exit 10; fi;
+        local bsh=$(which bash) 
+        eval "${bsh} ${script} ${options_args}" 
+    }
+
+    function _action_update() {
+        local basher=$(which bshr)
+        if [ ! "$basher" = "" ];
+        then
+            echo "Updating..."
+            local abspath=$(readlink "$basher")
+            curl -o "$abspath" https://raw.githubusercontent.com/dalibor91/basher/master/basher.sh
+            if [ $? -eq 0 ];
+            then
+                echo "Updated."
+            else
+                echo "Update failed"
+            fi
+        fi
+    }
+
+
+    if [ $options_help -eq 1 ]; then _action_help; fi;
+    if [ $options_list -eq 1 ]; then _action_list; fi;
+    if [ ! "$options_delete" = "" ]; then _action_remove $options_delete ; fi;
+    if [ ! "$options_add" = "" ]; then _action_add $options_add ; fi;
+    if [ ! "$options_explain" = "" ]; then _action_explain $options_explain ; fi;
+    if [ $options_update -eq 1 ]; 
+    then 
+        echo "Do update"
+        #_action_update
+    fi;
+    if [ ! "$options_remote" = "" ]; 
+    then 
+        _action_remote $options_run $options_remote; 
+    elif [ ! "$options_run" = "" ];
+    then 
+        _action_run $options_run 
+    fi
 }
 
-function update() {
-	local basher=$(which basher)
-	if [ ! "$basher" = "" ];
-	then
-		echo "Updating..."
-		local abspath=$(readlink "$basher")
-		curl -o "$abspath" https://raw.githubusercontent.com/dalibor91/basher/master/basher.sh
-		if [ $? -eq 0 ];
-		then
-			echo "Updated."
-		else
-			echo "Update failed"
-		fi
-	fi
-}
+#create directories 
+_checkDirectories
 
-function use() {
-local prog=$(basename $1)
-echo "$prog list
-	lists all availible options
-$prog add "url"
-	downloads script from url and saves it
-$prog run <alias> [arguments]
-	runs some script
-$prog explain <alias>
-	prints description and url we used to download this script
-$prog remove <alias>
-	removes some script
-$prog update
-	updates itself to the lates version
-	you have to run it as root
-	you must have curl installed";
-}
+#parse input params 
+_parseParams $@
 
-check $(dirname "${BASER_DIR}")
 
-if [ "$1" == "run" ];
-then
-	if [ ! -f "${BASER_DIR}/aliases/${2}" ];
-	then
-		echo "Alias does not exists"
-	fi;
+#echo "
+#    Add    = ${options_add}
+#    Remove = ${options_remove}
+#    Remote = ${options_remote}
+#    Run    = ${options_run}
+#    List   = ${options_list}
+#    Help   = ${options_help}
+#    Explain= ${options_explain}
+#    Update = ${options_update}
+#    Args   = ${options_args}
+#";
 
-	bash_script=$(readlink "${BASER_DIR}/aliases/${2}")
+_process
 
-	shift
-	shift
 
-	eval "/bin/bash ${bash_script} $@"
-elif [ "$1" == "list" ];
-then
-	echo "Availible scripts:"
-	for al in $(ls "${BASER_DIR}/aliases/");
-	do
-		echo -e "\t- ${al}"
-	done;
-elif [ "$1" == "add" ];
-then
-	if [[ "$2" =~ ^https?: ]];
-	then
-		fetch $2
-	else
-		fetch_local $2
-	fi
-elif [ "$1" == "explain" ];
-then
-	explain $2
-elif [ "$1" == "remove" ];
-then
-	remove $2
-elif [ "$1" == "update" ];
-then
-        update
-else
-	use $0
-fi;
+
+
+exit;

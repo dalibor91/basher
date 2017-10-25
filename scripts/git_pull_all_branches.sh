e95adb146b80bcba5acc4c07da81128f003fbc8b
dalibor91 <dalibor_menkovic@hotmail.com>
2017-10-25 10:54:02 +0200

    Update git_pull_all_branches.sh

--- scripts/git_pull_all_branches.sh
+++ scripts/git_pull_all_branches.sh
@@ -1,4 +1,24 @@
 #!/bin/bash
 
-for i in $(git branch -r | grep -v 'origin/HEAD'); do branch=$(echo $i | awk '{ a=$1; gsub("origin/", "", a); print a }'); git checkout $branch; git pull; done
+for i in $(git branch -r | grep -v 'origin/HEAD');
+do
+        branch=$(echo $i | awk '{ a=$1; gsub("origin/", "", a); print a }');
+        echo "Checkout ${branch}"
+        git checkout $branch > /dev/null 2>&1
+
+        if [ ! $? -eq 0 ];
+        then
+                echo "Unable to checkout ${branch}"
+                exit 1
+        fi
+
+        git pull > /dev/null 2>&1
+
+        if [ ! $? -eq 0 ];
+        then
+                echo "Unable to pull ${branch}"
+                exit 1
+        fi
+
+done
 git checkout master

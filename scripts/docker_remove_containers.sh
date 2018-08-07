c1b7ed1987b55e3280ebc38e65924c268813699d
dalibor91 <dalibor.menkovic@gmail.com>
2018-08-07 12:11:49 +0200

    Update docker_remove_containers.sh

--- scripts/docker_remove_containers.sh
+++ scripts/docker_remove_containers.sh
@@ -1,3 +1,15 @@
 #!/bin/bash 
 
+FILTER="name=$1"
 
+if [ "$1" = "" ];
+then 
+  echo "Sure to remove all ? Ctrl+C to abort"
+  read sure
+fi;
+
+for container in $(docker ps -a --filter "${FILTER}" --format "{{.ID}}");
+do 
+  echo "Removing ${container}";
+  docker rm -vf "${container}";
+done
